404d0497b98df6e6a022b7c422b919e12222419b
Lucas de Almeida Matos <45777124+LucasAMatos@users.noreply.github.com>
2025-07-12 12:17:41 -0300

    Criando implementação Inicial (#1)
    * [WIP] Implementação Inicial
    
    * [WIP] Inclusão de Nugets
    
    * [DEV] Finalizando Implementação Inicial
    
    finalizando implementação inicial com healthCheck e Swagger para teste
    
    * [FIX] Corrigindo ignore para não carregar arquivos de debug
    
    ---------
    
    Co-authored-by: Lucas Matos <lmatos@banksofttecnologia.com.br>

---
+++ ApiTaskManager/Helpers/ConfigurationHelper.cs
@@ -0,0 +1,38 @@
+﻿using ApiTaskManager.Middlewares;
+
+namespace ApiTaskManager.Helpers
+{
+    public static class ConfigurationHelper
+    {
+        public static IServiceCollection ConfigureApiServices(this IServiceCollection services)
+        {
+            services.AddEndpointsApiExplorer();
+            services.AddOpenApiDocument(config =>
+            {
+                config.Title = "AlteredSearch API";
+                config.Version = "v1";
+                config.Description = "API para consultar dados públicos do jogo Altered.";
+            });
+            // Add services to the container.
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            services.AddOpenApi();
+            services.AddHealthChecks();
+
+            return services;
+        }
+        public static WebApplication ConfigureApiPipeline(this WebApplication app)
+        {
+            app.UseMiddleware<ExceptionHandlingMiddleware>();
+            app.UseOpenApi();      // Gera o /swagger/v1/swagger.json
+            app.UseSwaggerUi();   // Interface Swagger compatível com WithDescription
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi(); 
+            }
+
+            return app;
+        }
+    }
+}
