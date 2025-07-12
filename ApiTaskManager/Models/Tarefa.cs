92c0c5c654287d5ec53b6efc23660ba606caedc0
Lucas de Almeida Matos <45777124+LucasAMatos@users.noreply.github.com>
2025-07-12 16:27:03 -0300

    [DEV] Criado classes iniciais do projeto
    Close #2
    
    Co-authored-by: Lucas Matos <lmatos@banksofttecnologia.com.br>

---
+++ ApiTaskManager/Models/Tarefa.cs
@@ -0,0 +1,15 @@
+﻿namespace ApiTaskManager.Models;
+public class Tarefa
+{
+    public int Id { get; set; }
+
+    public required string Titulo { get; set; }
+
+    public required string Descricao { get; set; }
+
+    public DateOnly DataDeVencimento { get; set; }
+
+    public int Status { get; set; }
+
+    public Usuario usuario { get; set; }
+}
