6a1e04b4edc3eed3eabe37c83d8b2f921f36f2ab
Lucas de Almeida Matos <45777124+LucasAMatos@users.noreply.github.com>
2025-07-12 12:19:05 -0300

    Criando implementação Inicial (#1)
    * [WIP] Implementação Inicial
    
    * [WIP] Inclusão de Nugets
    
    * [DEV] Finalizando Implementação Inicial
    
    finalizando implementação inicial com healthCheck e Swagger para teste
    
    * [FIX] Corrigindo ignore para não carregar arquivos de debug
    
    ---------
    
    Co-authored-by: Lucas Matos <lmatos@banksofttecnologia.com.br>

---
+++ ApiTaskManager/Endpoints/HealthCheckEndpoints.cs
@@ -0,0 +1,63 @@
+﻿using ApiTaskManager.OutPut;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.OpenApi.Models;
+
+namespace ApiTaskManager.Endpoints
+{
+    public static class HealthCheckEndpoints
+    {
+        public static void RegisterHealthChekEndpoints(this WebApplication api)
+        {
+            api.MapGet("hc", async (HttpContext httpContext,[FromServices] HealthCheckService healthCheckService) =>
+            {
+                var report = await healthCheckService.CheckHealthAsync();
+
+                var result = new
+                {
+                    status = report.Status.ToString(),
+                    checks = report.Entries.Select(entry => new
+                    {
+                        key = entry.Key,
+                        status = entry.Value.Status.ToString(),
+                        description = entry.Value.Description,
+                        data = entry.Value.Data
+                    }),
+                    warnings = report.Entries
+                    .Where(entry => entry.Value.Status == HealthStatus.Degraded)
+                    .Select(entry => new
+                    {
+                        key = entry.Key,
+                        message = entry.Value.Description
+                    })
+                };
+
+                int statusCode = StatusCodes.Status204NoContent;
+
+                switch (report.Status)
+                {
+                    case HealthStatus.Unhealthy:
+                        statusCode = StatusCodes.Status503ServiceUnavailable;
+                        break;
+                    case HealthStatus.Degraded:
+                        statusCode = StatusCodes.Status500InternalServerError;
+                        break;
+                    case HealthStatus.Healthy:
+                        statusCode = StatusCodes.Status200OK;
+                        break;
+                    default:
+                        break;
+                }
+            })
+            .WithOpenApi(operation => new(operation)
+            {
+                OperationId = "HealthCheck",
+                Summary = "Status da API",
+                Tags = new List<OpenApiTag> { new OpenApiTag { Name = "HealthCheck" } }
+            })
+            .Produces<OutputHealthCheck>(StatusCodes.Status200OK)
+            .Produces<OutputHealthCheck>(StatusCodes.Status500InternalServerError)
+            .Produces<OutputHealthCheck>(StatusCodes.Status503ServiceUnavailable);
+        }
+    }
+}
---
+++ ApiTaskManager/Program.cs
@@ -0,0 +1,16 @@
+using ApiTaskManager.Endpoints;
+using ApiTaskManager.Helpers;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.ConfigureApiServices();
+
+var app = builder.Build();
+
+app.ConfigureApiPipeline();
+
+app.UseHttpsRedirection();
+
+app.RegisterHealthChekEndpoints();
+
+app.Run();
