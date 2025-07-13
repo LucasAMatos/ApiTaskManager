bc45fe76cf4cbbbfa2603f73a452e7cafab50872
Lucas Matos <lmatos@banksofttecnologia.com.br>
2025-07-13 01:34:05 -0300

    [WIP] Migration da tabela de histórico

---
+++ ApiTaskManager/Migrations/20250713043315_create_historico.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiTaskManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class create_historico : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tarefas_Projetos_ProjetoId",
+                table: "Tarefas");
+
+            migrationBuilder.RenameColumn(
+                name: "usuario",
+                table: "Tarefas",
+                newName: "Usuario");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProjetoId",
+                table: "Tarefas",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Comentario",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    conteudo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Usuario = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IdTarefa = table.Column<int>(type: "int", nullable: false),
+                    TarefaId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comentario", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Comentario_Tarefas_TarefaId",
+                        column: x => x.TarefaId,
+                        principalTable: "Tarefas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TarefasHistorico",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdTarefa = table.Column<int>(type: "int", nullable: false),
+                    Titulo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DataDeVencimento = table.Column<DateOnly>(type: "date", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    Usuario = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Prioridade = table.Column<int>(type: "int", nullable: false),
+                    ProjetoId = table.Column<int>(type: "int", nullable: false),
+                    AlteradoPor = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DescricaoDaAlteracao = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DataAlteracao = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TarefasHistorico", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comentario_TarefaId",
+                table: "Comentario",
+                column: "TarefaId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tarefas_Projetos_ProjetoId",
+                table: "Tarefas",
+                column: "ProjetoId",
+                principalTable: "Projetos",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tarefas_Projetos_ProjetoId",
+                table: "Tarefas");
+
+            migrationBuilder.DropTable(
+                name: "Comentario");
+
+            migrationBuilder.DropTable(
+                name: "TarefasHistorico");
+
+            migrationBuilder.RenameColumn(
+                name: "Usuario",
+                table: "Tarefas",
+                newName: "usuario");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProjetoId",
+                table: "Tarefas",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tarefas_Projetos_ProjetoId",
+                table: "Tarefas",
+                column: "ProjetoId",
+                principalTable: "Projetos",
+                principalColumn: "Id");
+        }
+    }
+}
